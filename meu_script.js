c39015dc2eafd088d6b67a52d9dc30837961f0bb
Rafael-Caio <RafaelCaio.Tech@outlook.com>
2024-03-27 18:19:47 -0300

    javascript

---
+++ meu_script.js
@@ -0,0 +1,7 @@
+alert("ola, bem vindo ao curso!!!")
+// é usado para comentários de uma linha.
+/* usando ara comentarrios de multiplas linhas */
+
+/* usado para selecionar um elemento no DOM (document object model)
+   no caso o id="nome" e em seguida atribuir um valor a este elemento */
+document.getElementById("nome").value = "oi"
